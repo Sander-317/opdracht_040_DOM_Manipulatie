7d24f8688759df29bb9b63ab4b8d47f36aea906d
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-06-07 10:41:58 +0200

    Fixed the problem dank je wel Samantha

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const bigFiveButton = document.querySelectorAll(".big-five-button").forEach(button => {
-    button.addEventListener("click", bigFiveSpotted )
-})
+const bigFiveButton = document.querySelectorAll(".big-five-button")
+
+// .forEach(button => {
+//     button.addEventListener("click", bigFiveSpotted(button) )
+// })
+
+document.querySelectorAll(".big-five-button").forEach(button => button.addEventListener("click", () => bigFiveSpotted(button.innerHTML)))
 
 function bigFiveSpotted(animal) {
     console.log("works")
     // console.log(document.getElementsByTagName("button").innerText)
-    console.log(animal.getElementsByClassName(".big-five-button").innerText)
-    // console.log(document.getElementsById("lion").innerText)
-    // console.log(animal.innerText)
+    // console.log(document.getElementsByClassName(".big-five-button").innerHTML)
+    // console.log(animal.innerHTML)
+    console.log(animal)
 }
