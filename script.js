8b18efa6d7abd621aad0de5d2b65a975ee1ddc22
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-06-07 11:32:33 +0200

    added the remove item from list button and made it work

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const bigFiveButton = document.querySelectorAll(".big-five-button")
+const RemoveFirstItemButton = document.getElementById("remove-first-item-button")
 
 document.querySelectorAll(".big-five-button").forEach(button => button.addEventListener("click", () => bigFiveSpotted(button.innerHTML)))
+RemoveFirstItemButton.addEventListener("click", removeFirstItem)
 
 // const newLi = document.createElement("li")
 const spottedAnimalsList = document.getElementById("spotted-animals-list")
@@ -12,4 +14,8 @@ function bigFiveSpotted(animal) {
     newLi.innerHTML = `${animal}`
     console.log(newLi)
     spottedAnimalsList.appendChild(newLi)
+}
+
+function removeFirstItem() {
+   spottedAnimalsList.removeChild(spottedAnimalsList.childNodes[0])
 }
