5cb206e6941405e9e8aec3bf03de30eb99c4ccd0
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-06-07 09:54:27 +0200

    updated to ask for help

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const bigFiveButton = document.querySelectorAll(".big-five-button")
-
-// bigFiveButton.forEach( () => {
-//     button.addEventListener("click", bigFiveSpotted)
-// })
-
-
-bigFiveButton.addEventListener("click",bigFiveSpotted)
+const bigFiveButton = document.querySelectorAll(".big-five-button").forEach(button => {
+    button.addEventListener("click", bigFiveSpotted )
+})
 
 function bigFiveSpotted(animal) {
     console.log("works")
+    // console.log(document.getElementsByTagName("button").innerText)
+    console.log(animal.getElementsByClassName(".big-five-button").innerText)
+    // console.log(document.getElementsById("lion").innerText)
+    // console.log(animal.innerText)
 }
-
