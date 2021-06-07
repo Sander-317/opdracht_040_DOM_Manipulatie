85451bd5da553a2b87b0939fd4b689bded301498
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-06-07 09:29:30 +0200

    added event listners and made function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("test")
+const bigFiveButton = document.querySelectorAll(".big-five-button")
+
+// bigFiveButton.forEach( () => {
+//     button.addEventListener("click", bigFiveSpotted)
+// })
+
+
+bigFiveButton.addEventListener("click",bigFiveSpotted)
+
+function bigFiveSpotted(animal) {
+    console.log("works")
+}
+
