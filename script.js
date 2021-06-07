a7c7a5c105840257e6e0f3f1ca7d0666ebfe1c71
Sander van Meerveld <sander.van.meerveld@gmail.com>
2021-06-07 11:22:12 +0200

    added newLi to spotted animal list

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const bigFiveButton = document.querySelectorAll(".big-five-button")
 
 document.querySelectorAll(".big-five-button").forEach(button => button.addEventListener("click", () => bigFiveSpotted(button.innerHTML)))
 
-const newLi = document.createElement("li")
+// const newLi = document.createElement("li")
+const spottedAnimalsList = document.getElementById("spotted-animals-list")
+
 
 function bigFiveSpotted(animal) {
     console.log(animal)
     let newLi = document.createElement("li")
     newLi.innerHTML = `${animal}`
     console.log(newLi)
+    spottedAnimalsList.appendChild(newLi)
 }
